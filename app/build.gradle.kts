57b0f095bb5da49ca9917673897232d4b275afdf
Levostap <levostapenkov@gmail.com>
2023-12-26 02:09:17 +0300

    History fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("com.google.code.gson:gson:2.8.9")
     implementation(project(":Domain"))
     implementation(project(":Data"))
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
--- app/src/main/java/com/euphoiniateam/euphonia/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/euphoiniateam/euphonia/ui/history/HistoryFragment.kt
@@ -1,17 +1,28 @@
 package com.euphoiniateam.euphonia.ui.history
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.euphoiniateam.euphonia.R
 import com.euphoiniateam.euphonia.databinding.FragmentHistoryBinding
+import com.google.gson.Gson
+
 
 class HistoryFragment : Fragment() {
 
     private var _binding: FragmentHistoryBinding? = null
+
+    lateinit var searchList: ListView
+    lateinit var listAdapter: ArrayAdapter<String>
+    var dataList = arrayListOf<String>()
+    lateinit var searchView: SearchView
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,21 +33,75 @@ class HistoryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val historyViewModel =
-            ViewModelProvider(this).get(HistoryViewModel::class.java)
 
-        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val rootView = inflater.inflate(R.layout.fragment_history, container, false)
+        searchList = rootView.findViewById(R.id.searchResults)
+        searchView = rootView.findViewById(R.id.idSV)
 
-        val textView: TextView = binding.textDashboard
-        historyViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val sharedPreferences = requireContext().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        if (sharedPreferences.contains(KEY_ARRAY)) {
+            val savedArray = getArrayFromSharedPreferences(requireContext())
+            for(i in savedArray.indices){
+                dataList.add(savedArray[i])
+            }
+        } else {
+            val gson = Gson()
+            dataList.add("MyPlay")
+            dataList.add("CoolSound")
+            dataList.add("MyPlay1")
+            dataList.add("Krutoten'")
+            dataList.add("MyPlay2")
+            dataList.add("MyPlay3")
+            dataList.add("MyPlay4")
+            dataList.add("Лунная соната Людвиг ван Бетховен.")
+            val json = gson.toJson(dataList)
+            editor.putString(KEY_ARRAY, json)
+            editor.apply()
         }
-        return root
+
+        listAdapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_list_item_1,
+            dataList
+        )
+        searchList.adapter = listAdapter
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (dataList.contains(query)) {
+                    listAdapter.filter.filter(query)
+                }
+                return false
+            }
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listAdapter.filter.filter(newText)
+                return false
+            }
+        })
+
+        return rootView
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun getArrayFromSharedPreferences(context: Context): Array<String> {
+        val sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+
+        return if (sharedPreferences.contains(KEY_ARRAY)) {
+            val gson = Gson()
+            val json = sharedPreferences.getString(KEY_ARRAY, null)
+            gson.fromJson(json, Array<String>::class.java) ?: emptyArray()
+        } else {
+            emptyArray()
+        }
+    }
+    companion object{
+        private const val PREF_NAME = "Music"
+        private const val KEY_ARRAY = "MusicVault"
+    }
 }
+
