157268eff48f1c847a833e48f3c85935da531ab0
Nitro <nitro@git.com>
2024-02-21 18:51:04 +0300

    Open file mock functionality

--- app/src/main/java/com/euphoiniateam/euphonia/ui/home/HomeFragment.kt
+++ app/src/main/java/com/euphoiniateam/euphonia/ui/home/HomeFragment.kt
@@ -1,18 +1,32 @@
 package com.euphoiniateam.euphonia.ui.home
 
+import android.Manifest
+import android.app.Activity
+import android.content.ContentProvider
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.transition.TransitionInflater
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.CallSuper
+import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import com.euphoiniateam.euphonia.databinding.FragmentHomeBinding
+import java.io.File
+
 
 class HomeFragment : Fragment() {
 
@@ -21,6 +35,35 @@ class HomeFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+//    private val mPermissionResult: ActivityResultLauncher<String> = registerForActivityResult(
+//        ActivityResultContracts.RequestPermission(),
+//        {  if(it) {
+//            Log.e("TAG", "onActivityResult: PERMISSION GRANTED");
+//        } else {
+//            Log.e("TAG", "onActivityResult: PERMISSION DENIED");
+//        }}
+//    )
+val getContent = registerForActivityResult(
+    ActivityResultContracts.GetContent()) { uri: Uri? ->
+    if(uri != null){
+        val fileName = uri
+        Toast.makeText(context, "Открыта файл: $fileName", Toast.LENGTH_SHORT).show()
+    }
+}
+
+    val requstPermission = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),{ isGranted ->
+            if (isGranted) {
+                getContent.launch("*/*")
+            } else {
+                Toast.makeText(context, "permisson not granted", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    )
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,11 +78,17 @@ class HomeFragment : Fragment() {
         val animation = TransitionInflater.from(requireContext()).inflateTransition(android.R.transition.move)
         sharedElementEnterTransition = animation
 
+
+
         val btnOpenPiano: ImageButton = binding.btnToPiano
         btnOpenPiano.setOnClickListener {
             val action = HomeFragmentDirections.actionNavigationHomeToPianoFragment()
             val navController = findNavController()
             navController.navigate(action)
+        }
+
+        binding.btnToFile.setOnClickListener {
+            getFileReadPermission()
         }
 
         val btnStartMicro: ImageButton = binding.btnToMicro
@@ -56,6 +105,13 @@ class HomeFragment : Fragment() {
 
         return root
     }
+    fun getFileReadPermission(){
+        requstPermission.launch(Manifest.permission.READ_MEDIA_AUDIO)
+
+        }
+
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
