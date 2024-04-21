c0a807fd64bf1e7a4665b8317ab88e1998b500b5
mrcrashban <metelevkirill4@gmail.com>
2024-04-21 16:55:15 +0300

    add functions to file StorageFunctions.kt

--- app/src/main/java/com/euphoiniateam/euphonia/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/euphoiniateam/euphonia/ui/history/HistoryFragment.kt
@@ -1,8 +1,6 @@
 package com.euphoiniateam.euphonia.ui.history
 
-import android.content.Context
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +10,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.euphoiniateam.euphonia.databinding.FragmentHistoryBinding
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
+import com.euphoiniateam.euphonia.tools.getMidFileNamesFromPiano
 
 class HistoryFragment : Fragment() {
 
@@ -33,24 +29,14 @@ class HistoryFragment : Fragment() {
 
         _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val rootView = binding.root
-
         recyclerView = binding.searchResults
         searchView = binding.idSV
-
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         listAdapter = HistoryAdapter(
-            getMidFileNamesFromExternalStorage(),
+            getMidFileNamesFromPiano(),
             findNavController()
         )
         recyclerView.adapter = listAdapter
-
-        /*val dir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Euphonia")
-        dir.mkdirs()
-        saveMidiToExternalStorage(requireContext(), R.raw.bumer, dir, "bumer.mid")*/
-        //val midiResourceId = R.raw.angra_carolina_iv
-        //val midiInputStream: InputStream = resources.openRawResource(midiResourceId)
-        //saveMidiToInternalStorage(requireContext(), midiInputStream, "angra_carolina_iv.mid")
-
         searchView.setOnQueryTextListener(
             object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
@@ -70,28 +56,5 @@ class HistoryFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    private fun getMidFileNamesFromExternalStorage(): ArrayList<String> {
-        val dir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "Euphonia")
-        dir.mkdirs()
-        val midiFilesNames = ArrayList<String>()
-        dir.listFiles()?.forEach { file ->
-            if (file.isFile && file.extension.equals("mid", ignoreCase = true)) {
-                midiFilesNames.add(file.name)
-            }
-        }
-        return midiFilesNames
-    }
-
-    //Функция для сохранения midi файлов в internal storage
-    private fun saveMidiToExternalStorage(context: Context, resourceId: Int, targetDirectory: File, fileName: String) {
-        val inputStream: InputStream = context.resources.openRawResource(resourceId)
-        val outputStream = FileOutputStream(File(targetDirectory, fileName))
-        inputStream.use { input ->
-            outputStream.use { output ->
-                input.copyTo(output)
-            }
-        }
     }
 }
