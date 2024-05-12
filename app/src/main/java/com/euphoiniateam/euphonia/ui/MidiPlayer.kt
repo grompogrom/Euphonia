8c43f1a8864a248aa8ef85742dcbe2a87817f637
Vladimir Gromov <gromPoGrom@mail.ru>
2024-05-12 17:40:55 +0300

    rewrite-detection

--- app/src/main/java/com/euphoiniateam/euphonia/ui/MidiPlayer.kt
+++ app/src/main/java/com/euphoiniateam/euphonia/ui/MidiPlayer.kt
@@ -16,23 +16,25 @@ class MidiPlayer {
     }
 
     fun initWithTrack(context: Context, midiUri: Uri) {
+        Log.d("MidiPlayer", "Init with track. Current uri: $midiUri")
+
         currentUri = midiUri
         mediaPlayer.reset()
         mediaPlayer.setDataSource(context, midiUri)
         mediaPlayer.prepare()
-        Log.d("AAA", "Player prepared")
+        Log.d("MidPlayer", "Player prepared")
     }
 
     fun play(context: Context, midiUri: Uri) {
         initWithTrack(context, midiUri)
         playerState.tryEmit(true)
         mediaPlayer.start()
-        Log.d("AAA", "Player started. Current uri: $midiUri")
+        Log.d("MidiPlayer", "Player started. Current uri: $midiUri")
     }
     fun stop() {
         mediaPlayer.stop()
         playerState.tryEmit(false)
-        Log.d("AAA", "Player stopped")
+        Log.d("MidiPlayer", "Player stopped")
     }
 
     fun release() = mediaPlayer.release()
