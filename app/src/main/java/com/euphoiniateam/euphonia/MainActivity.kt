b22fc2021ae8c7d4a61064ce121ae7d620e45196
bigronimax <ronimax@bk.ru>
2024-05-16 23:32:33 +0300

    no Vk auth(

--- app/src/main/java/com/euphoiniateam/euphonia/MainActivity.kt
+++ app/src/main/java/com/euphoiniateam/euphonia/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.euphoiniateam.euphonia
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.euphoiniateam.euphonia.databinding.ActivityMainBinding
-import com.euphoiniateam.euphonia.domain.models.VKUser
-import com.euphoiniateam.euphonia.ui.settings.VKUsersCommand
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.vk.api.sdk.VK
-import com.vk.api.sdk.VKApiCallback
-import com.vk.api.sdk.VKTokenExpiredHandler
-import com.vk.api.sdk.auth.VKAuthenticationResult
-import com.vk.api.sdk.auth.VKScope
 
 // TODO:  Дополнительно
 // 1. Удалить мусор Data/Domain
@@ -37,20 +28,6 @@ class MainActivity : AppCompatActivity() {
 //                Log.d("AAA", str.toString())
 //        }
 
-        val authLauncher = VK.login(this) { result: VKAuthenticationResult ->
-            when (result) {
-                is VKAuthenticationResult.Success -> {
-                    // Toast.makeText(this, "YESYESYESYES", Toast.LENGTH_SHORT).show()
-                    VK.addTokenExpiredHandler(tokenTracker)
-                    requestUser()
-                }
-                is VKAuthenticationResult.Failed -> {
-                    Toast.makeText(this, "NONONONO", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-        authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS))
-
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -68,31 +45,5 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private val tokenTracker = object : VKTokenExpiredHandler {
-        override fun onTokenExpired() {
-            // token expired
-        }
-    }
 
-    private fun requestUser() {
-        VK.execute(
-            VKUsersCommand(),
-            object : VKApiCallback<List<VKUser>> {
-                override fun success(result: List<VKUser>) {
-                    if (result.isNotEmpty()) {
-                        val user = result[0]
-                        val username = "${user.firstName} ${user.lastName}"
-                        Toast.makeText(
-                            applicationContext,
-                            "Welcome, $username!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-                override fun fail(error: Exception) {
-                    Log.e("VKRequest", error.toString())
-                }
-            }
-        )
-    }
 }
