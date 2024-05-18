3ec100202c833a65646956af80e260525a3a7081
bigronimax <ronimax@bk.ru>
2024-05-18 13:50:55 +0300

    Vk response user data, friends data

--- app/src/main/java/com/euphoiniateam/euphonia/MainActivity.kt
+++ app/src/main/java/com/euphoiniateam/euphonia/MainActivity.kt
@@ -15,6 +15,9 @@ import com.vk.api.sdk.VK
 import com.vk.api.sdk.VKApiCallback
 import com.vk.api.sdk.VKTokenExpiredHandler
 import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.sdk.api.friends.FriendsService
+import com.vk.sdk.api.friends.dto.FriendsGetFieldsResponseDto
+import com.vk.sdk.api.users.dto.UsersFieldsDto
 
 // TODO:  Дополнительно
 // 1. Удалить мусор Data/Domain
@@ -22,11 +25,15 @@ import com.vk.api.sdk.auth.VKAuthenticationResult
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var user: VKUser? = null
+    private var friends: List<VKUser>? = null
+
     val authLauncher = VK.login(this) { result: VKAuthenticationResult ->
         when (result) {
             is VKAuthenticationResult.Success -> {
                 VK.addTokenExpiredHandler(tokenTracker)
-                requestUser()
+                user = requestUser()
+                friends = requestFriends()
             }
 
             is VKAuthenticationResult.Failed -> {
@@ -71,25 +78,76 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun requestUser() {
+    private fun requestUser(): VKUser? {
+        var user: VKUser? = null
         VK.execute(
             VKUsersCommand(),
             object : VKApiCallback<List<VKUser>> {
                 override fun success(result: List<VKUser>) {
-                    if (result.isNotEmpty()) {
-                        val user = result[0]
-                        val username = "${user.firstName} ${user.lastName}"
+                    if (!isFinishing && result.isNotEmpty()) {
+                        user = result[0]
+                        val username = "${user!!.firstName} ${user!!.lastName}"
                         Toast.makeText(
                             this@MainActivity,
                             "Welcome, $username!",
                             Toast.LENGTH_SHORT
                         ).show()
+                        showUser(user!!)
                     }
                 }
                 override fun fail(error: Exception) {
-                    Log.e("VKRequest", error.toString())
+                    Log.e("userRequest", error.toString())
                 }
             }
         )
+        return user
+    }
+
+    private fun requestFriends(): List<VKUser>? {
+        var friends: List<VKUser>? = null
+        val fields = listOf(UsersFieldsDto.PHOTO_200, UsersFieldsDto.BDATE)
+        VK.execute(
+            FriendsService().friendsGet(fields = fields),
+            object : VKApiCallback<FriendsGetFieldsResponseDto> {
+                override fun success(result: FriendsGetFieldsResponseDto) {
+                    val friendsRequest = result.items
+                    if (!isFinishing && friendsRequest.isNotEmpty()) {
+                        friends = friendsRequest.map { friend ->
+                            VKUser(
+                                id = friend.id.value,
+                                firstName = friend.firstName ?: "",
+                                lastName = friend.lastName ?: "",
+                                birthDate = friend.bdate ?: "",
+                                photo = friend.photo200 ?: "",
+                            )
+                        }
+                        showFriends(friends!!)
+                    }
+                }
+                override fun fail(error: Exception) {
+                    Log.e("friendsRequest", error.toString())
+                }
+            }
+        )
+        return friends
+    }
+
+    private fun showFriends(friends: List<VKUser>) {
+        Log.d("friend", friends.size.toString())
+        for (friend in friends) {
+            Log.d("friend", friend.id.toString())
+            Log.d("friend", friend.firstName)
+            Log.d("friend", friend.lastName)
+            Log.d("friend", friend.birthDate)
+            Log.d("friend", friend.photo)
+        }
+    }
+
+    private fun showUser(user: VKUser) {
+        Log.d("user", user.id.toString())
+        Log.d("user", user.firstName)
+        Log.d("user", user.lastName)
+        Log.d("user", user.birthDate)
+        Log.d("user", user.photo)
     }
 }
